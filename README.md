f32b956d0a9754eec3d94f0607f2d6912c2289b0
Richard Schneider <makaretu@gmail.com>
2016-10-24 14:16:07 +1300

    feat(units): extensibility
    units() - get a list of unit symbols
    addUnit(unit, factor) - add a new unit
    removeUnit(unit) - remove a unit

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Conversion between the bitcoin base unit (BTC) and other units (Satoshi,  μBTC,
 - Converts from/to a [Number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number), 
   [Big](https://www.npmjs.com/package/big.js) or 
   [String](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)
-
+- Units can be added or removed
 
 ## Getting started
 
@@ -58,3 +58,34 @@ Converts an amount from one unit to another unit.
 
     btcConvert(2, 'BTC', 'bits') // returns 2000000
     btcConvert(2, 'BTC', 'bits', 'String') // returns '2000000'
+    
+### btcConvert.units()
+
+Returns an Array of unit symbols that can be used for conversion.
+
+#### Example
+
+    console.log(btcConvert.units())
+    // produces
+    // ['BTC', 'mBTC', 'μBTC', 'bits', 'bit', 'Satoshi', 'sat']
+    
+### btcConvert.addUnit (unit, factor)
+
+Adds a new `unit` for conversion.  Throws when `unit` already exists and the factors are different.
+
+- `unit` (string) - the new unit symbol
+- `factor` (number | string | Big) - conversion factor to yield a `BTC`.
+
+#### Example
+
+    convert.addUnit('finney', 0.0000001);
+    convert(30, 'finney', 'BTC') // produces 0.000003
+    
+    // 10 satoshis can be expressed as 1 finney
+    convert(20, 'Satoshi', 'finney') // produces 2
+
+### btcConvert.removeUnit (unit)
+
+Removes the `unit` from conversion.  Throws when `unit` is pre-defined. Removing a non-existing symbol is allowed.
+
+- `unit` (string) - the unit symbol to remove.
--- test/units.js
+++ test/units.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const convert = require('..');
-const Big = require('big.js');
 const should = require('should');
 
 describe('Units', () => {
@@ -14,5 +13,41 @@ describe('Units', () => {
         units.should.containEql('Satoshi');
     });
 
+    describe('extensibility', () => {
+        it('should add a new unit', () => {
+            convert.units().should.not.containEql('finney');
+            convert.addUnit('finney', 0.0000001);
+            convert.units().should.containEql('finney');
+            // 10 satoshis can be expressed as 1 finney
+            convert(20, 'Satoshi', 'finney').should.equal(2);
+            convert.removeUnit('finney');
+        });
+
+        it('should not add an existing unit with a different conversion factor', () => {
+            convert.units().should.containEql('BTC');
+            convert.addUnit('BTC', 1);
+            should.throws(() => convert.addUnit('BTC', 3));
+        });
+
+        it('should remove a new unit', () => {
+            convert.units().should.not.containEql('finney');
+            convert.addUnit('finney', 0.0000001);
+            convert.units().should.containEql('finney');
+            convert.removeUnit('finney');
+            convert.units().should.not.containEql('finney');
+        });
+
+        it('should remove a non-existent unit', () => {
+            convert.units().should.not.containEql('x');
+            convert.removeUnit('x');
+            convert.units().should.not.containEql('x');
+        });
+
+        it('should not remove a predefined unit', () => {
+            convert.units().should.containEql('BTC');
+            should.throws(() => convert.removeUnit('BTC'));
+        });
+
+    });
 
 });
